d466f182f10b78d9ea9b0e303ac23d1a01d38cfb
porico <one.yukiko@gmail.com>
2018-04-13 19:02:51 +0900

    4.1　データの受け渡し

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
+// [Functional Component]
+const Hello = (props) => {
+  return <div>Hello {props.name} !</div>;
+}
+
+// [Class Component]
+// - React.Componentを明示的に継承し、propsはthis.propsで受けとる
+// - stateがある
+// - ライフサイクルメソッドを定義することができる
 class App extends Component {
   render() {
-    const fullnames = {
-      ryoma: '坂本龍馬',
-      taka: '西郷隆盛',
-      kai: '勝海舟'
-    };
-    const getFullname = nickname => fullnames[nickname];
-    const element = <h2>Hello, {getFullname('ryoma')}</h2>
-    const list = (
-      <ul>
-        {[1,2,3].map(num => <li>{num}</li>)}
-      </ul>
-    )
-    
-    const heading = React.createElement(
-      "h3",
-      null,
-      "h3 text"
-    );
-    
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    //return <div>Hello {this.props.name} !!</div>;
+    return(
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Hello World!</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
 
-        { heading }
-    
-        { element }
-        { list }
-      </div>
-    );
+        <ul>
+          <li>no: {this.props.no}</li>
+          <li>name: {this.props.name}</li>
+          <li>birth day: {this.props.birthDay}</li>
+        </ul>
+    )
   }
 }
+
+
+
+// [型情報の追加]
+// PropTypesは、インポートしたprop-typesモジュールが持つオブジェクトで方情報を持っている
+// propTypesは、自作したReactコンポーネントが持つプロパティ名で、各propsの型情報を記述する
+Hello.propTypes = {
+  name: PropTypes.string
+};
+
+App.propTypes = {
+  name: PropTypes.string.isRequired,
+  no: PropTypes.number.isRequired,
+  hogeBool: PropTypes.bool,
+  hogeArray: PropTypes.array,
+  hogeObject: PropTypes.object,
+  hogeFunc: PropTypes.func,
+  hogeSymbol: PropTypes.symbol
+  // element, node, instanceOf, oneOf, oneOfType, etc..
+};
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const profile = [
+  {
+    no: 1,
+    name: 'Yamada',
+    birthDay: '1900/10/4'
+  },{
+    no: 2,
+    name: 'Tanaka',
+    birthDay: '2000/2/3'
+  }
+];
+
 ReactDOM.render(
-  <App />, 
-  document.getElementById('root'));
+  <React.Fragment>
+
+    <p>Reactコンポーネントは単一の親からなる要素しか表現できない。ので要素を並列で作成する場合は、Fragmentコンポーネントを利用する。</p>
+<code>
+&lt;React.Fragment&gt;&lt;/React.Fragment&gt;
+<br/>
+    または、簡略記法（新しい）
+<br/>
+&lt;&gt;&lt;/&gt;
+</code>
+    
+    <p>以下はすべてReactエレメント</p>
+    <App {...profile[0]} /> //「...」はオブジェクトの中身を展開して渡す
+    <App {...profile[1]} />
+    <App no={3} name="Yukiko" birthDay="1975/7/14" />
+    <App no={4} name="Tomoko" birthDay="1978/1/1" />
+  </React.Fragment>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
